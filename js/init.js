aa7c3e65d0b72bf00c276eb2ba6d4133d001e134
Chris Wainner <chris.jwainner@gmail.com>
2017-05-04 14:52:41 -0700

    Update folder structure, add extra stylings

--- js/init.js
+++ js/init.js
@@ -4,29 +4,5 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-    var modal = document.getElementById('aboutSectionModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("aboutSection");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
   }); // end of document ready
 })(jQuery); // end of jQuery name space
